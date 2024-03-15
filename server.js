07d8dd6aeb681d336ab4395045b012a6636abcd2
Azzam Izzudin Hasan <88259831+ZamIzzudin@users.noreply.github.com>
2024-03-15 15:01:13 +0700

    cord(change cors to wildcard)

--- server.js
+++ server.js
@@ -22,8 +22,8 @@ app.use('/public', express.static(path.join(__dirname, '/public')))
 //enable cors
 app.use(cors({
     credentials: true,
-    // origin: "*"
-    origin: ['https://www.tedxuinjakarta.com','https://tedxuinjakarta.com','https://dashboard.tedxuinjakarta.com','https://tedxuinjakarta.vercel.app','https://dashboard-tedxuinjakarta.vercel.app','https://api.tedxuinjakarta.com','http://localhost:8000','http://localhost:3000','http://localhost:3001']
+    origin: "*"
+    // origin: ['https://www.tedxuinjakarta.com','https://tedxuinjakarta.com','https://dashboard.tedxuinjakarta.com','https://tedxuinjakarta.vercel.app','https://dashboard-tedxuinjakarta.vercel.app','https://api.tedxuinjakarta.com','http://localhost:8000','http://localhost:3000','http://localhost:3001']
     // origin: ['https://tedxuinjakarta.com','https://www.tedxuinjakarta.com', 'https://dashboard.tedxuinjakarta.com', 'https://tedxuinjakarta.vercel.app', 'http://localhost:3000', 'http://localhost:3001','https://dashboard-tedxuinjakarta.vercel.app']
 }))
 
