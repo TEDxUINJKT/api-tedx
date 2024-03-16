bde81eea202d898849b79fd811043eccb8e07840
ZamIzzudin <azzamizzudinhasan@gmail.com>
2024-03-16 20:15:08 +0700

    feat:(add redirect feature on ticket)

--- src/controllers/event.js
+++ src/controllers/event.js
@@ -1,379 +1,410 @@
-const Event = require('../models/event.js')
-const Ticket = require('../models/ticket.js')
-const { upload, destroy } = require('../libs/fileHandler.js')
-const {encrypt} = require('../libs/crypto.js')
+/** @format */
+
+const Event = require("../models/event.js");
+const Ticket = require("../models/ticket.js");
+const { upload, destroy } = require("../libs/fileHandler.js");
+const { encrypt } = require("../libs/crypto.js");
 
 const get_event = async (req, res) => {
-    const { version } = req.params
-    try {
-        const data = await Event.find({ version }).sort({created_at:-1})
+  const { version } = req.params;
+  try {
+    const data = await Event.find({ version }).sort({ created_at: -1 });
 
-        return res.status(200).json({
-            status: 200,
-            message: "Success Get Event List",
-            events: data
-        })
-    } catch (err) {
-        return res.status(500).json({
-            status: 500,
-            message: 'failed',
-            info: 'server error',
-            stack: err
-        })
-    }
-}
+    return res.status(200).json({
+      status: 200,
+      message: "Success Get Event List",
+      events: data,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      status: 500,
+      message: "failed",
+      info: "server error",
+      stack: err,
+    });
+  }
+};
 
 const get_detail_event = async (req, res) => {
-    const { id } = req.params
-    try {
-        const data = await Event.findOne({ _id: id })
+  const { id } = req.params;
+  try {
+    const data = await Event.findOne({ _id: id });
 
-        if (!data) {
-            return res.status(403).json({
-                status: 403,
-                message: 'failed',
-                info: 'Cannot Find Event'
-            })
-        } else {
-            return res.status(200).json({
-                status: 200,
-                message: "Success Get Event Detail",
-                detail: data
-            })
-        }
-    } catch (err) {
-        return res.status(500).json({
-            status: 500,
-            message: 'failed',
-            info: 'server error',
-            stack: err
-        })
+    if (!data) {
+      return res.status(403).json({
+        status: 403,
+        message: "failed",
+        info: "Cannot Find Event",
+      });
+    } else {
+      return res.status(200).json({
+        status: 200,
+        message: "Success Get Event Detail",
+        detail: data,
+      });
     }
-}
+  } catch (err) {
+    return res.status(500).json({
+      status: 500,
+      message: "failed",
+      info: "server error",
+      stack: err,
+    });
+  }
+};
 
 const get_ticket_detail = async (req, res) => {
-    const { id } = req.params
-    try {
-        const ticket = await Ticket.findOne({ _id: id })
+  const { id } = req.params;
+  try {
+    const ticket = await Ticket.findOne({ _id: id });
 
-        if (!ticket) {
-            return res.status(403).json({
-                status: 403,
-                message: 'failed',
-                info: 'Cannot Find Ticket'
-            })
-        } else {
-            const event = await Event.findOne({_id:ticket.event_id})
+    if (!ticket) {
+      return res.status(403).json({
+        status: 403,
+        message: "failed",
+        info: "Cannot Find Ticket",
+      });
+    } else {
+      const event = await Event.findOne({ _id: ticket.event_id });
 
-            const plain = JSON.stringify(ticket)
-            const parsed = JSON.parse(plain)
+      const plain = JSON.stringify(ticket);
+      const parsed = JSON.parse(plain);
 
-            return res.status(200).json({
-                status: 200,
-                message: "Success Get Ticket Detail",
-                detail: {
-                    ticket:{...parsed,refferal:encrypt(JSON.stringify(parsed.refferal))},
-                    event
-                }
-            })
-        }
-    } catch (err) {
-        return res.status(500).json({
-            status: 500,
-            message: 'failed',
-            info: 'server error',
-            stack: err
-        })
+      return res.status(200).json({
+        status: 200,
+        message: "Success Get Ticket Detail",
+        detail: {
+          ticket: {
+            ...parsed,
+            refferal: encrypt(JSON.stringify(parsed.refferal)),
+          },
+          event,
+        },
+      });
     }
-}
+  } catch (err) {
+    return res.status(500).json({
+      status: 500,
+      message: "failed",
+      info: "server error",
+      stack: err,
+    });
+  }
+};
 
 const get_ticket_list = async (req, res) => {
-    const { id } = req.params
-    try {
-        const data = await Ticket.find({ event_id: id,is_publish:true }).sort({created_at:-1})
+  const { id } = req.params;
+  try {
+    const data = await Ticket.find({ event_id: id, is_publish: true }).sort({
+      created_at: -1,
+    });
 
-        const new_data = data.map((each) => {
-            const plain = JSON.stringify(each)
-            const parsed = JSON.parse(plain)
-            return {
-                ...parsed,refferal:encrypt(JSON.stringify(each.refferal))
-            }
-        })
+    const new_data = data.map((each) => {
+      const plain = JSON.stringify(each);
+      const parsed = JSON.parse(plain);
+      return {
+        ...parsed,
+        refferal: encrypt(JSON.stringify(each.refferal)),
+      };
+    });
 
-        return res.status(200).json({
-            status: 200,
-            message: "Success Get Ticket List",
-            tickets: new_data
-        })
-    } catch (err) {
-        return res.status(500).json({
-            status: 500,
-            message: 'failed',
-            info: 'server error',
-            stack: err
-        })
-    }
-}
+    return res.status(200).json({
+      status: 200,
+      message: "Success Get Ticket List",
+      tickets: new_data,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      status: 500,
+      message: "failed",
+      info: "server error",
+      stack: err,
+    });
+  }
+};
 
 const get_ticket_all_list = async (req, res) => {
-    const { id } = req.params
-    try {
-        const data = await Ticket.find({ event_id: id}).sort({created_at:-1})
+  const { id } = req.params;
+  try {
+    const data = await Ticket.find({ event_id: id }).sort({ created_at: -1 });
 
-        return res.status(200).json({
-            status: 200,
-            message: "Success Get Ticket List",
-            tickets: data
-        })
-    } catch (err) {
-        return res.status(500).json({
-            status: 500,
-            message: 'failed',
-            info: 'server error',
-            stack: err
-        })
-    }
-}
+    return res.status(200).json({
+      status: 200,
+      message: "Success Get Ticket List",
+      tickets: data,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      status: 500,
+      message: "failed",
+      info: "server error",
+      stack: err,
+    });
+  }
+};
 
 const add_event = async (req, res) => {
-    const { event, description, date, time, place, version, type } = req.body
-    try {
-        let payload = {
-            event,
-            description,
-            date,
-            time,
-            place,
-            version,
-            type
-        }
+  const { event, description, date, time, place, version, type } = req.body;
+  try {
+    let payload = {
+      event,
+      description,
+      date,
+      time,
+      place,
+      version,
+      type,
+    };
 
-        if (req.files) {
-            // Upload New Thumbnail
-            const { file } = req.files
-            const { url_picture, url_public } = await upload(file)
+    if (req.files) {
+      // Upload New Thumbnail
+      const { file } = req.files;
+      const { url_picture, url_public } = await upload(file);
 
-            payload['thumbnail'] = {
-                public_id: url_public,
-                url: url_picture
-            }
-        }
-
-        const data = await Event.create(payload)
-
-        if (!data) {
-            return res.status(403).json({
-                status: 403,
-                message: 'failed',
-                info: 'Cannot Add New Event'
-            })
-        } else {
-            return res.status(200).json({
-                status: 200,
-                message: "Success Add New Event",
-            })
-        }
-    } catch (err) {
-        return res.status(500).json({
-            status: 500,
-            message: 'failed',
-            info: 'server error',
-            stack: err
-        })
+      payload["thumbnail"] = {
+        public_id: url_public,
+        url: url_picture,
+      };
     }
-}
+
+    const data = await Event.create(payload);
+
+    if (!data) {
+      return res.status(403).json({
+        status: 403,
+        message: "failed",
+        info: "Cannot Add New Event",
+      });
+    } else {
+      return res.status(200).json({
+        status: 200,
+        message: "Success Add New Event",
+      });
+    }
+  } catch (err) {
+    return res.status(500).json({
+      status: 500,
+      message: "failed",
+      info: "server error",
+      stack: err,
+    });
+  }
+};
 
 const add_ticket = async (req, res) => {
-    const { id } = req.params
-    const { type_ticket,bundle_status,quota,is_publish, description, price, status, refferal } = req.body
-    try {
-        const payload = {
-            event_id: id,
-            type_ticket,
-            description,
-            bundle_status,
-            quota,
-            is_publish,
-            price,
-            status,
-            refferal
-        }
+  const { id } = req.params;
+  const {
+    type_ticket,
+    bundle_status,
+    quota,
+    is_publish,
+    description,
+    price,
+    status,
+    refferal,
+    redirect = "",
+  } = req.body;
+  try {
+    const payload = {
+      event_id: id,
+      type_ticket,
+      description,
+      bundle_status,
+      quota,
+      is_publish,
+      price,
+      status,
+      refferal,
+      redirect,
+    };
 
-        const data = await Ticket.create(payload)
+    const data = await Ticket.create(payload);
 
-        if (!data) {
-            return res.status(403).json({
-                status: 403,
-                message: 'failed',
-                info: 'Cannot Add New Ticket'
-            })
-        } else {
-            return res.status(200).json({
-                status: 200,
-                message: "Success Add New Ticket"
-            })
-        }
-    } catch (err) {
-        return res.status(500).json({
-            status: 500,
-            message: 'failed',
-            info: 'server error',
-            stack: err
-        })
+    if (!data) {
+      return res.status(403).json({
+        status: 403,
+        message: "failed",
+        info: "Cannot Add New Ticket",
+      });
+    } else {
+      return res.status(200).json({
+        status: 200,
+        message: "Success Add New Ticket",
+      });
     }
-}
+  } catch (err) {
+    return res.status(500).json({
+      status: 500,
+      message: "failed",
+      info: "server error",
+      stack: err,
+    });
+  }
+};
 
 const update_event = async (req, res) => {
-    const { id } = req.params
-    const { event, description, date, time, place, version, type } = req.body
-    try {
-        let payload = {
-            event,
-            description,
-            date,
-            time,
-            place,
-            version,
-            type
-        }
+  const { id } = req.params;
+  const { event, description, date, time, place, version, type } = req.body;
+  try {
+    let payload = {
+      event,
+      description,
+      date,
+      time,
+      place,
+      version,
+      type,
+    };
 
-        if (req.files) {
-            // Upload New Thumbnail
-            const { file } = req.files
-            const { url_picture, url_public } = await upload(file)
+    if (req.files) {
+      // Upload New Thumbnail
+      const { file } = req.files;
+      const { url_picture, url_public } = await upload(file);
 
-            payload['thumbnail'] = {
-                public_id: url_public,
-                url: url_picture
-            }
+      payload["thumbnail"] = {
+        public_id: url_public,
+        url: url_picture,
+      };
 
-            // Delete Exisisting Thumbnail
-            const existing = await Event.findOne({ _id: id }, { thumbnail: 1 })
-            destroy(existing.thumbnail.public_id)
-        }
-
-        const data = await Event.updateOne({ _id: id }, payload)
-
-
-        if (!data.modifiedCount) {
-            return res.status(403).json({
-                status: 403,
-                message: 'failed',
-                info: 'Cannot Update Event'
-            })
-        } else {
-            return res.status(200).json({
-                status: 200,
-                message: "Success Update Event",
-            })
-        }
-    } catch (err) {
-        return res.status(500).json({
-            status: 500,
-            message: 'failed',
-            info: 'server error',
-            stack: err
-        })
+      // Delete Exisisting Thumbnail
+      const existing = await Event.findOne({ _id: id }, { thumbnail: 1 });
+      destroy(existing.thumbnail.public_id);
     }
-}
+
+    const data = await Event.updateOne({ _id: id }, payload);
+
+    if (!data.modifiedCount) {
+      return res.status(403).json({
+        status: 403,
+        message: "failed",
+        info: "Cannot Update Event",
+      });
+    } else {
+      return res.status(200).json({
+        status: 200,
+        message: "Success Update Event",
+      });
+    }
+  } catch (err) {
+    return res.status(500).json({
+      status: 500,
+      message: "failed",
+      info: "server error",
+      stack: err,
+    });
+  }
+};
 
 const update_ticket = async (req, res) => {
-    const { id } = req.params
-    const { type_ticket,bundle_status,quota, is_publish,description, price, status, refferal } = req.body
-    try {
-        let payload = {
-            type_ticket,
-            description,
-            bundle_status,
-            quota,
-            is_publish,
-            price,
-            status,
-            refferal
-        }
+  const { id } = req.params;
+  const {
+    type_ticket,
+    bundle_status,
+    quota,
+    is_publish,
+    description,
+    price,
+    status,
+    refferal,
+    redirect = "",
+  } = req.body;
+  try {
+    let payload = {
+      type_ticket,
+      description,
+      bundle_status,
+      quota,
+      is_publish,
+      price,
+      status,
+      refferal,
+      redirect,
+    };
 
-        const data = await Ticket.updateOne({ _id: id }, payload)
+    console.log(payload);
 
-        if (!data.modifiedCount) {
-            return res.status(403).json({
-                status: 403,
-                message: 'failed',
-                info: 'Cannot Update Ticket'
-            })
-        } else {
-            return res.status(200).json({
-                status: 200,
-                message: "Success Update Ticket",
-            })
-        }
-    } catch (err) {
-        return res.status(500).json({
-            status: 500,
-            message: 'failed',
-            info: 'server error',
-            stack: err
-        })
+    const data = await Ticket.updateOne({ _id: id }, payload);
+
+    if (!data.modifiedCount) {
+      return res.status(403).json({
+        status: 403,
+        message: "failed",
+        info: "Cannot Update Ticket",
+      });
+    } else {
+      return res.status(200).json({
+        status: 200,
+        message: "Success Update Ticket",
+      });
     }
-}
+  } catch (err) {
+    return res.status(500).json({
+      status: 500,
+      message: "failed",
+      info: "server error",
+      stack: err,
+    });
+  }
+};
 
 const delete_event = async (req, res) => {
-    const { id } = req.params
-    try {
-        // Delete Existing Image
-        const existing = await Event.findOne({ _id: id }, { thumbnail: 1 })
-        destroy(existing.thumbnail.public_id)
+  const { id } = req.params;
+  try {
+    // Delete Existing Image
+    const existing = await Event.findOne({ _id: id }, { thumbnail: 1 });
+    destroy(existing.thumbnail.public_id);
 
-        const event = await Event.deleteOne({ _id: id })
+    const event = await Event.deleteOne({ _id: id });
 
-        if (!event.deletedCount) {
-            return res.status(403).json({
-                status: 403,
-                message: 'failed',
-                info: 'Cannot Delete Event'
-            })
-        } else {
-            await Ticket.deleteMany({ event_id: id })
-            return res.status(200).json({
-                status: 200,
-                message: "Success Delete Event and Ticket"
-            })
-        }
-    } catch (err) {
-        return res.status(500).json({
-            status: 500,
-            message: 'failed',
-            info: 'server error',
-            stack: err
-        })
+    if (!event.deletedCount) {
+      return res.status(403).json({
+        status: 403,
+        message: "failed",
+        info: "Cannot Delete Event",
+      });
+    } else {
+      await Ticket.deleteMany({ event_id: id });
+      return res.status(200).json({
+        status: 200,
+        message: "Success Delete Event and Ticket",
+      });
     }
-}
+  } catch (err) {
+    return res.status(500).json({
+      status: 500,
+      message: "failed",
+      info: "server error",
+      stack: err,
+    });
+  }
+};
 
 const delete_ticket = async (req, res) => {
-    const { id } = req.params
-    try {
-        const data = await Ticket.deleteOne({ _id: id })
+  const { id } = req.params;
+  try {
+    const data = await Ticket.deleteOne({ _id: id });
 
-        if (!data.deletedCount) {
-            return res.status(403).json({
-                status: 403,
-                message: 'failed',
-                info: 'Cannot Delete Ticket'
-            })
-        } else {
-            return res.status(200).json({
-                status: 200,
-                message: "Success Delete Ticket"
-            })
-        }
-    } catch (err) {
-        return res.status(500).json({
-            status: 500,
-            message: 'failed',
-            info: 'server error',
-            stack: err
-        })
+    if (!data.deletedCount) {
+      return res.status(403).json({
+        status: 403,
+        message: "failed",
+        info: "Cannot Delete Ticket",
+      });
+    } else {
+      return res.status(200).json({
+        status: 200,
+        message: "Success Delete Ticket",
+      });
     }
-}
+  } catch (err) {
+    return res.status(500).json({
+      status: 500,
+      message: "failed",
+      info: "server error",
+      stack: err,
+    });
+  }
+};
 
 // const name = async (req,res) => {
 //     try{
@@ -390,4 +421,16 @@ const delete_ticket = async (req, res) => {
 //     }
 // }
 
-module.exports = { get_event, get_detail_event,get_ticket_detail,get_ticket_all_list, get_ticket_list, add_event, add_ticket, update_event, update_ticket, delete_event, delete_ticket }
+module.exports = {
+  get_event,
+  get_detail_event,
+  get_ticket_detail,
+  get_ticket_all_list,
+  get_ticket_list,
+  add_event,
+  add_ticket,
+  update_event,
+  update_ticket,
+  delete_event,
+  delete_ticket,
+};
--- src/models/ticket.js
+++ src/models/ticket.js
@@ -1,37 +1,43 @@
-const mongoose = require('mongoose')
+/** @format */
+
+const mongoose = require("mongoose");
 
 const ticket_scheme = new mongoose.Schema({
-    event_id: String,
-    type_ticket: String,
-    description: String,
-    price: Number,
-    quota:Number,
-    bundle_status:{
-        type:Object,
-        default:{
-            is_bundle:false,
-            bundle_count:1
-        }
+  event_id: String,
+  type_ticket: String,
+  description: String,
+  price: Number,
+  quota: Number,
+  bundle_status: {
+    type: Object,
+    default: {
+      is_bundle: false,
+      bundle_count: 1,
     },
-    status: {
-        type: String,
-        enum: ['Available', 'Sold Out'],
-        default: 'Available'
-    },
-    is_publish:{
-        type:Boolean,
-        default:true
-    },
-    refferal: {
-        type: Array,
-        default: []
-    },
-    created_at: {
-        type: Date,
-        default: new Date()
-    }
-})
+  },
+  status: {
+    type: String,
+    enum: ["Available", "Sold Out"],
+    default: "Available",
+  },
+  is_publish: {
+    type: Boolean,
+    default: true,
+  },
+  redirect: {
+    type: String,
+    default: "",
+  },
+  refferal: {
+    type: Array,
+    default: [],
+  },
+  created_at: {
+    type: Date,
+    default: new Date(),
+  },
+});
 
-const Ticket = mongoose.model("Ticket", ticket_scheme)
+const Ticket = mongoose.model("Ticket", ticket_scheme);
 
-module.exports = Ticket
+module.exports = Ticket;
